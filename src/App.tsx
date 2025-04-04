a5a20b0e9f2446fc29432c0fbca79f49cc8bdb4e
itseduardolima <eduardolima2417@gmail.com>
2025-04-04 05:27:55 -0400

    feat: config initial

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Home from "./pages/Home";
+import "./styles/main.scss";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#17876D",
+    },
+    secondary: {
+      main: "#00AAC1",
+    },
+    error: {
+      main: "#B00020",
+    },
+    background: {
+      default: "#F0F0F0",
+    },
+  },
+  typography: {
+    fontFamily: "Manrope, sans-serif",
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: "none",
+          borderRadius: 8,
+        },
+      },
+    },
+  },
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
